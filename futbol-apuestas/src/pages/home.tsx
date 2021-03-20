27020784185d773bd3a018bab306c73c0051cc4a
JustDancePC <jesusdavid268@outlook.com>
2021-03-19 20:12:21 -0400

    removidos imports sin uso

--- futbol-apuestas/src/pages/home.tsx
+++ futbol-apuestas/src/pages/home.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import MenuComponent from '../components/pageHeader';
-import {PageHeader} from 'antd';
 
 import type { RootState } from '../state-store/reducer.root';
 import type { Session } from '../state-store/session/session.types';
@@ -28,15 +26,6 @@ type Props = PropsFromRedux & {
 }
 
 function Home(props: Props) {
-
-    let history = useHistory();
-
-    const [imgWidth, setImgWidth] = useState('100%');
-
-    useEffect(() => {
-        if (props.width > 769) { setImgWidth('40%') }
-        else { setImgWidth('95%') }
-    }, [props.width])
 
     return (
         <div>
