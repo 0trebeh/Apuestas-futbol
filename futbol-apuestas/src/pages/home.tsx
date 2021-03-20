d1fa4c76701688e4ca3b97583627d882ab0c3802
JustDancePC <jesusdavid268@outlook.com>
2021-03-19 22:05:30 -0400

    Unas imagenes en el home. Pa que se vea mas mejor

--- futbol-apuestas/src/pages/home.tsx
+++ futbol-apuestas/src/pages/home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import MenuComponent from '../components/pageHeader';
-
+import { Card, Carousel, Image } from 'antd';
+import { LoadingOutlined, ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
 import type { RootState } from '../state-store/reducer.root';
 import type { Session } from '../state-store/session/session.types';
 
@@ -30,6 +31,83 @@ function Home(props: Props) {
     return (
         <div>
             <MenuComponent sessionActive={props.sessionActive} />
+            <Carousel
+                autoplay={true}
+                draggable={true}
+                swipeToSlide={true}
+                adaptiveHeight={true}
+                dots={true}
+                dotPosition={'bottom'}
+            >
+                <Card
+                    hoverable={true}
+                    cover={
+                        <Image
+                            placeholder={
+                                <LoadingOutlined spin={true} />
+                            }
+                            preview={false}
+                            src={'https://media.foxdeportes.com/photos/cache/1024x574/photos/2018/02/07/5a7ba71046f7c.jpeg'}
+                        />
+                    }
+                >
+                    <Card.Meta
+                        title={'Partidos'}
+                        description={'Encuentra informacion sobre partidos, ligas, torneos, etc.'}
+                    />
+                </Card>
+                <Card
+                    hoverable={true}
+                    cover={
+                        <Image
+                            placeholder={
+                                <LoadingOutlined spin={true} />
+                            }
+                            preview={false}
+                            src={'http://www.mundoesotericoparanormal.com/wp-content/uploads/2015/10/loteria-vision-remota.jpg'}
+                        />
+                    }
+                >
+                    <Card.Meta
+                        title={'Predicciones'}
+                        description={'Encuentra las mejores predicciones calculadas por nuestro clon de Albert Einstein'}
+                    />
+                </Card>
+                <Card
+                    hoverable={true}
+                    cover={
+                        <Image
+                            placeholder={
+                                <LoadingOutlined spin={true} />
+                            }
+                            preview={false}
+                            src={'https://www.rushbet.co/blog/wp-content/uploads/2020/12/shutterstock_1505491208-1.jpg'}
+                        />
+                    }
+                >
+                    <Card.Meta
+                        title={'Apuestas'}
+                        description={'Apuesta por tus equipos y/o jugadores favoritos'}
+                    />
+                </Card>
+                <Card
+                    hoverable={true}
+                    cover={
+                        <Image
+                            placeholder={
+                                <LoadingOutlined spin={true} />
+                            }
+                            preview={false}
+                            src={'http://4.bp.blogspot.com/-XslnkJaFqNA/UdjF4FchoHI/AAAAAAAAOHU/7bkDnpZ78KA/s1600/lionel+messi+bicycle+kick+photo+moment+5.jpg'}
+                        />
+                    }
+                >
+                    <Card.Meta
+                        title={'Estadisticas'}
+                        description={'Encuentra estadisticas generales y por partidos de equipos y jugadores'}
+                    />
+                </Card>
+            </Carousel>
         </div>
     )
 }
