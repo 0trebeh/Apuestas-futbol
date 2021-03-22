5d67ac38077d5366a69a4b4fab730e90bdaa6dee
JustDancePC <jesusdavid268@outlook.com>
2021-03-21 23:54:16 -0400

    prueba 2

--- src/server.js
+++ src/server.js
@@ -2,19 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const userRoutes = require('./routes/users');
-
 //inicializaciones
 const server = express();
 
 //Middlewares
+//server.use(helmet());
 server.use(cors({origin: 'http://localhost:3000'}));
-server.use(helmet());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 //rutas
-server.use('/api/users', userRoutes);
+server.use('/api/users', require('./routes/users'));
 server.use('/api/league', require('./routes/league'));
 
 module.exports = server;
