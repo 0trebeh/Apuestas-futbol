6aeb9a14d38b587237295caee082a3103f6d45c4
JustDancePC <jesusdavid268@outlook.com>
2021-03-21 23:31:15 -0400

    origin added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
     console.log('Server in port: ' + PORT);
