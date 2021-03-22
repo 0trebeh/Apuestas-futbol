309717e010fc27edb023f565e46563a58953f69b
JustDancePC <jesusdavid268@outlook.com>
2021-03-22 00:00:12 -0400

    prueba 3

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const server = express();
 
 //Middlewares
-//server.use(helmet());
+server.use(helmet());
 server.use(cors({origin: 'http://localhost:3000'}));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
