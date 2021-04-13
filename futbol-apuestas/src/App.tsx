ba0db83d0519f103479ba85361d63950dd50452d
0trebeh <hebertourribarri2@gmail.com>
2021-04-13 12:43:15 -0400

    Creacion Apuestas, Estadisticas, Predicciones, Partidos. Mostrando Hola mundo por los momentos

--- futbol-apuestas/src/App.tsx
+++ futbol-apuestas/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {persistor, store} from './state-store/store';
 import axios from 'axios';
+
+import Home from './pages/home';
 import Profile from './pages/profile';
+import Apuestas from './pages/apuestas';
+import Estadisticas from './pages/estisticas';
+import Predicciones from './pages/predicciones';
+import Partidos from './pages/partidos';
 
 axios.defaults.baseURL = 'https://fap-api.herokuapp.com';
 
@@ -21,6 +26,18 @@ function App() {
             </Route>
             <Route exact path={'/profile'}>
               <Profile />
+            </Route>
+            <Route exact path={'/apuestas'}>
+              <Apuestas />
+            </Route>
+            <Route exact path={'/estadisticas'}>
+              <Estadisticas />
+            </Route>
+            <Route exact path={'/predicciones'}>
+              <Predicciones />
+            </Route>
+            <Route exact path={'/partidos'}>
+              <Partidos />
             </Route>
           </Switch>
         </BrowserRouter>
---
+++ futbol-apuestas/src/pages/partidos.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {connect, ConnectedProps} from 'react-redux';
+import MenuComponent from '../components/pageHeader';
+import {Card} from 'antd';
+//import {LoadingOutlined} from '@ant-design/icons';
+
+import type {RootState} from '../state-store/reducer.root';
+import type {Session} from '../state-store/session/session.types';
+
+const mapStateToProps = (state: RootState) => ({
+  username: state.session.session.username,
+  sessionActive: state.session.session.isSessionActive,
+});
+const mapDispatchToProps = {
+  setData: (data: Session) => ({
+    type: 'SAVE_SESSION_DATA',
+    data: data,
+  }),
+};
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = PropsFromRedux;
+
+function Partidos(props: Props) {
+  return (
+    <div>
+      <MenuComponent sessionActive={props.sessionActive} />
+        <Card.Meta
+            title={'Partidos'}
+            description={
+                'Partidos'
+            }
+        />
+    </div>
+  );
+}
+
+export default connector(Partidos);
